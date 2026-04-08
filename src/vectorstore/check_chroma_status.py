fb97325782bbcc1d91fa382f03dbbaf4f4a8d1ee
Choon Leng <73415759+ChoonLeng773@users.noreply.github.com>
2026-04-08 19:10:45 +0800

    test: checked if database works and is returning data

--- src/vectorstore/check_chroma_status.py
+++ src/vectorstore/check_chroma_status.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import chromadb
+from sentence_transformers import SentenceTransformer
 
 PERSIST_DIR = "data/vectors"
 CHROMA_COLLECTION_NAME = "my_knowledge_base"
@@ -14,4 +15,17 @@ print(collection.count())  # should be > 0
 
 # embeddings exist for my database -> search function to be updated
 result = collection.get(limit=1, include=["embeddings"])
-print(result["embeddings"])
+# print(result["embeddings"])
+
+query = """I am a fresh grad who just started working, and i would like to buy a house.
+What schemes can be helpful to me and my girlfriend?"""
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+query_vector = model.encode([query]).tolist()
+print(len(query_vector))
+query_answer = collection.query(
+    query_embeddings=query_vector[0],
+    n_results=3,
+    include=["documents", "embeddings", "metadatas"],
+)
+print(query_answer)
